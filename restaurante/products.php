4129ba7f3e24c1a8b786c9da6e920144b20f642f
jescolar10 <javierescolar10@gmail.com>
2016-07-10 11:34:17 +0000

    se ha modificado la vista productos para hacerla más responsive mediante div´s

--- restaurante/products.php
+++ restaurante/products.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Title of the document</title>
+    <title>App - Restaurante</title>
     <link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" />
     <link rel="stylesheet" href="css/styles.css" type="text/css" />
     <link rel="stylesheet" href="css/styles-mobile.css" type="text/css" />
@@ -12,7 +12,7 @@
 </head>
 
 <body>
-<div class="container-fluid">
+    <div class="container-fluid">
   <?php
     include 'restaurante/partials/nav.php'
   ?>
@@ -32,48 +32,45 @@
       <div class="formularios row col-xs-12 col-md-8 col-md-offset-2">
           
           <div class="text-center cabeceraForm">Productos</div>
-          <form action='index.php' method='POST'>
-           <table class="table table-resposive table-condensed table-hover" id="tablaProductos">
-               <thead>
-                   <tr>
-                       <th class="text-center">EAN</th>
-                       <th class="text-center">Nombre</th>
-                       <th class="text-center">Cantidad</th>
-                       <th class="text-center">Caducidad</th>
-                       <th class="text-center" colspan="2">Acción</th>
-                   </tr>
-               </thead>
-               <tbody>
-                    <?php
-                       $datosProductos = [
-                           ["EAN"=>"EAN1","Nombre"=>"Nombre1","Cantidad"=>"Cantidad1","Caducidad"=>"Caducidad1"],
-                           ["EAN"=>"EAN2","Nombre"=>"Nombre2","Cantidad"=>"Cantidad2","Caducidad"=>"Caducidad2"],
-                           ["EAN"=>"EAN3","Nombre"=>"Nombre3","Cantidad"=>"Cantidad3","Caducidad"=>"Caducidad3"],
-                           ["EAN"=>"EAN4","Nombre"=>"Nombre4","Cantidad"=>"Cantidad4","Caducidad"=>"Caducidad4"],
-                           ["EAN"=>"EAN5","Nombre"=>"Nombre5","Cantidad"=>"Cantidad5","Caducidad"=>"Caducidad5"],
-                           ["EAN"=>"EAN6","Nombre"=>"Nombre6","Cantidad"=>"Cantidad6","Caducidad"=>"Caducidad6"],
-                           ["EAN"=>"EAN7","Nombre"=>"Nombre7","Cantidad"=>"Cantidad7","Caducidad"=>"Caducidad7"],
-                           ["EAN"=>"EAN8","Nombre"=>"Nombre8","Cantidad"=>"Cantidad8","Caducidad"=>"Caducidad8"],
-                       ];
+          <form action='index.php' method='POST' id="formProductos">
+              <div class="form-group row col-md-offset-1 col-md-10 cabeceraProductos">
+                  <div class="col-xs-12 col-md-1 col-md-offset-1">
+                      <h4>EAN</h4>
+                  </div>
+                  <div class="col-xs-12 col-md-1 col-md-offset-1">
+                      <h4>Nombre</h4>
+                  </div>
+                  <div class="col-xs-12 col-md-1 col-md-offset-1">
+                      <h4>Cantidad</h4>
+                  </div>
+                  <div class="col-xs-12 col-md-1 col-md-offset-1">
+                      <h4>Caducidad</h4>
+                  </div>
+                  <div class="col-xs-12 col-md-1 col-md-offset-1">
+                      <h4>Acción</h4>
+                  </div>
+              </div>
+               
+                    <?php  
                        foreach ($datosProductos as $fila) {
-                           echo "<tr>";
+                           echo '<div class="form-group row col-md-offset-1">';
                           foreach ($fila as $columna=>$celda) {
-                             echo "<td><input name='$columna' class='form-control' type='text' value='$celda'/></td>";
+                             echo '<div class="col-xs-12 col-md-2">'; 
+                             echo "<input name='$columna' class='form-control' type='text' value='$celda'/>";
+                             echo '</div>';
                           }
-                          echo "<td><button type='submit' name='saveProduct' class='btn-link save'><span class='glyphicon glyphicon-ok'></span></button></td>"
-                               ."<td><button type='submit' name='dropProduct' class='btn-link drop'><span class='glyphicon glyphicon-remove'></span></button></td>"
-                               ."</tr>";
+                           echo '<div class="col-xs-12 col-md-3">';
+                          echo "<button type='submit' name='saveProduct' class='btn-link save'><span class='glyphicon glyphicon-ok'></span></button>"
+                               ."<button type='submit' name='dropProduct' class='btn-link drop'><span class='glyphicon glyphicon-remove'></span></button>";
+                           echo '</div>';
+                          echo '</div>';
                        }
-
                    ?>
-               </tbody>
-
-           </table>
          </form>
            <button class="btn-link" id="nuevoProducto">añadir nuevo producto</button>
       </div>
   </section>
-
+</div>
 </body>
     <script type="text/javascript" src="js/jquery-1.12.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
